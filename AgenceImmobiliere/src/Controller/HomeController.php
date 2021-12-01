8c404185d68188d3c82ff02f624efdf0c2a4b1f7
Alkadios <lucas.delefortrie@free.fr>
2021-12-01 14:02:30 +0100

    Création entity property, migration bdd, affichage annonce, installation cocur/slug

--- AgenceImmobiliere/src/Controller/HomeController.php
+++ AgenceImmobiliere/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,11 +12,18 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     *
+     * @param PropertyRepository $repository
+     *
+     * @return Response
      */
-    public function index(): Response
+    public function index(PropertyRepository $repository) : Response
     {
+        $properties = $repository->findLatest();
+
         return $this->render('home/index.html.twig', [
-        'current_menu' => 'accueil'
+            'current_menu' => 'accueil',
+            'properties' => $properties
         ]);
     }
 }
--- AgenceImmobiliere/src/Controller/PropertyController.php
+++ AgenceImmobiliere/src/Controller/PropertyController.php
@@ -2,19 +2,51 @@
 
 namespace App\Controller;
 
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PropertyController extends AbstractController
 {
+
+    public function __construct(PropertyRepository $repository, EntityManagerInterface $manager)
+    {
+        $this->repository = $repository;
+        $this->manager = $manager;
+    }
+
     /**
      * @Route("/properties", name="properties_index")
+     *
+     * @return Response
      */
     public function index(): Response
     {
         return $this->render('property/index.html.twig', [
             'current_menu' => 'properties'
+        ]);
+    }
+
+    /**
+     * @Route("/properti/{id}/{slug}", name="property_show", requirements={"slug": "[a-z0-9\-]*" })
+     *
+     * @return Response
+     */
+    public function show(Property $property, $slug): Response
+    {
+        if($property->getSlug() !== $slug){
+            return $this->redirectToRoute('property_show', [
+                'id' => $property->getId(),
+                'slug' => $property->getSlug()
+            ], 301);
+        }
+
+         return $this->render('property/show.html.twig', [
+            'current_menu' => 'properties',
+             'property' => $property
         ]);
     }
 }
